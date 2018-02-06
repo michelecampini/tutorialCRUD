0ea684b814e72ac1b5157d2425c21f1c24ea6973
Alberto Mauri <alberto.mauri@live.com>
2018-02-06 17:08:50 +0100

    Merge branch 'albemau5-branch'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NaviComponent } from './navi/navi.component';
+import { PlacesComponent } from './places/places.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NaviComponent
+    NaviComponent,
+    PlacesComponent
   ],
   imports: [
     BrowserModule
